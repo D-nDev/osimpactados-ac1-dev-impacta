f4b10875a11705cbf4bdb9b34e887f63b6302776
D-nDev <diego-s.novaes@hotmail.com>
2022-09-29 04:22:38 -0300

    fix: fix getusers dependency

--- src/main/factories/GetUsersControllerFactory.ts
+++ src/main/factories/GetUsersControllerFactory.ts
@@ -1,12 +1,12 @@
 import GetUsersUseCase from '@usecases/getUsersUseCase';
 import GetUsersController from '@presentation/controllers/GetUsersController';
 import { BaseController } from '@presentation/controllers/contracts/BaseController';
-import { userRepositoryInstance } from '@shared/container';
+import { pinoAdapterInstance, userRepositoryInstance } from '@shared/container';
 
 export const getUsersControllerFactory = (): BaseController => {
   const useCase = new GetUsersUseCase(userRepositoryInstance);
 
-  const controller = new GetUsersController(useCase);
+  const controller = new GetUsersController(useCase, pinoAdapterInstance);
 
   return controller;
 };
